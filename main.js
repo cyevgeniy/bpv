d4b4a4d4196d867fc40bca407045962550140300
Evgeny Chaban <ychbn@ychbn.com>
2023-06-10 19:26:07 +0300

    feat: add 'set' command

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { bumpAllFiles, isDirty, tag, isUsable } from "./git.js";
 import process from "node:process";
 import { bump, parse, versionToString } from "./sv.js";
 import { replaceInFile } from "./replace.js";
+import { notOk } from "./result.js";
 import cac from "cac";
 
 const CONF_FILE = "bp.conf.json";
@@ -20,12 +21,30 @@ cli.option("--major", "Bump major version number")
 	.option("-c, --commit", "Commit after bump")
 	.option("-t, --tag", "Create a tag in the git repository");
 
-cli.command("bump").action(run);
+cli.command("bump").action(runBump);
+cli.command("set <version>").action(runSet);
 
-cli.parse();
+try {
+	cli.parse(process.argv, {run: false});
+
+	await cli.runMatchedCommand();
+} catch (/** @type {*}*/ error) {
+	printErrorAndExit(error.message)
+}
 
 /**
- * Returns regexp
+ * Prints error message and perform exit with status = 1.
+ *
+ * @param {string} errorMessage Error message
+ */
+function printErrorAndExit(errorMessage) {
+	console.error(errorMessage);
+	process.exit(1);
+}
+
+/**
+ * Takes template string from `config.rule` and returns
+ * a regexp for replacement.
  *
  * @param {string} versionTemplate - string with version template
  * @param {string} version - version in a string format
@@ -59,16 +78,77 @@ function printReplaceResults(replaceResult) {
 }
 
 /**
+ * Replace version in a file with a new value.
+ *
+ * @param {string} file - File path
+ * @param {string} currentVersion - Current version
+ * @param {string} newVersion - New version
+ * @param {string} pattern - Version string pattern
+ * @returns {Promise<import("./result.js").Result>}
+ */
+async function replaceVersionInFile(file, currentVersion, newVersion, pattern) {
+	const fromRegExp = getReplaceRegexp(pattern, currentVersion);
+
+	if (!fromRegExp) {
+		return notOk("Can't construct a regular expression for replacement");
+	}
+
+	/**
+	 * @type {import ('./replace.js').ReplaceOption}
+	 */
+	const replaceOption = {
+		file: file,
+		from: fromRegExp,
+		to: pattern.replaceAll("{{version}}", newVersion),
+	};
+
+	return await replaceInFile(replaceOption);
+}
+
+/**
+ * Replace version in all files from the `config.rules` array.
+ * If any errors were thrown during replacement, prints error
+ * message and calls `process.exit(1)`.
+ *
+ * @param {import("./config.js").Rule[]} rules Array of rules
+ * @param {string} currentVersion Current version
+ * @param {string} newVersion New version
+ * @param {boolean} printResults If true, prints replace results
+ */
+async function makeReplacements(
+	rules,
+	currentVersion,
+	newVersion,
+	printResults
+) {
+	for (const rule of rules) {
+		const result = await replaceVersionInFile(
+			rule.file,
+			currentVersion,
+			newVersion,
+			rule.version
+		);
+
+		if (!result.ok) {
+			printErrorAndExit(result.message);
+		}
+
+		if (printResults) {
+			printReplaceResults(result.value);
+		}
+	}
+}
+
+/**
  * Main function
  *
  * @param {any} options command-line options
  */
-async function run(options) {
+async function runBump(options) {
 	const configResult = loadConfig(CONF_FILE);
 
 	if (!configResult.ok) {
-		console.error(configResult.message);
-		process.exit(1);
+		printErrorAndExit(configResult.message);
 	}
 
 	const config = configResult.value;
@@ -77,22 +157,24 @@ async function run(options) {
 	const parseResult = parse(config.currentVersion);
 
 	if (!parseResult.ok) {
-		console.error(parseResult.message);
-		process.exit(1);
+		printErrorAndExit(parseResult.message);
 	}
 
 	if ((options.commit || options.tag) && !isUsable()) {
-		console.error(
+		printErrorAndExit(
 			"Can't use git because current directory is not inside a git repository"
 		);
-		process.exit(1);
 	}
 
 	if (options.commit && isDirty()) {
-		console.error("Can't commit because the repository has modified files");
-		process.exit(1);
+		printErrorAndExit(
+			"Can't commit because the repository has modified files"
+		);
 	}
 
+	/**
+	 * @type {import("./sv.js").Version}
+	 */
 	let newVersion = parseResult.value;
 
 	/**
@@ -107,39 +189,12 @@ async function run(options) {
 		}
 	}
 
-	for (const rule of config.rules) {
-		const fromRegExp = getReplaceRegexp(
-			rule.version,
-			config.currentVersion
-		);
-
-		if (!fromRegExp) {
-			continue;
-		}
-
-		/**
-		 * @type {import ('./replace.js').ReplaceOption}
-		 */
-		const replaceOption = {
-			file: rule.file,
-			from: fromRegExp,
-			to: rule.version.replaceAll(
-				"{{version}}",
-				versionToString(newVersion)
-			),
-		};
-
-		const result = await replaceInFile(replaceOption);
-
-		if (!result.ok) {
-			console.error(result.message);
-			process.exit(1);
-		}
-
-		if (options.verbose) {
-			printReplaceResults(result.value);
-		}
-	}
+	makeReplacements(
+		config.rules,
+		config.currentVersion,
+		versionToString(newVersion),
+		options.verbose
+	);
 
 	if (options.commit) {
 		bumpAllFiles();
@@ -147,5 +202,59 @@ async function run(options) {
 
 	if (options.tag) {
 		tag(versionToString(newVersion));
+	}
+}
+
+/**
+ * Set the specified version
+ *
+ * @param {string} version - New version
+ * @param {*} [options] - Command line options
+ */
+async function runSet(version, options) {
+	const parseResult = parse(version);
+
+	if (!parseResult.ok) {
+		printErrorAndExit(parseResult.message);
+	}
+
+	/**
+	 * @type {import("./sv.js").Version}
+	 */
+	const parsedVersion = parseResult.value;
+
+	const configResult = loadConfig(CONF_FILE);
+
+	if (!configResult.ok) {
+		printErrorAndExit(configResult.message);
+	}
+
+	const config = configResult.value;
+
+	if ((options.commit || options.tag) && !isUsable()) {
+		printErrorAndExit(
+			"Can't use git because current directory is not inside a git repository"
+		);
+	}
+
+	if (options.commit && isDirty()) {
+		printErrorAndExit(
+			"Can't commit because the repository has modified files"
+		);
+	}
+
+	makeReplacements(
+		config.rules,
+		config.currentVersion,
+		version,
+		options.verbose
+	);
+
+	if (options.commit) {
+		bumpAllFiles();
+	}
+
+	if (options.tag) {
+		tag(versionToString(parsedVersion));
 	}
 }
